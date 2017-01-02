c744917f15b69fc5fc61b5d35383de79df50b135
Levi Bostian <levi@curiosityio.com>
2017-01-02 13:23:28 -0600

    Ask for SMS read permission.

--- app/src/main/java/com/curiosityio/smsreadapp/MainActivity.java
+++ app/src/main/java/com/curiosityio/smsreadapp/MainActivity.java
@@ -1,13 +1,41 @@
 package com.curiosityio.smsreadapp;
 
+import android.Manifest;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.github.kayvannj.permission_utils.Func;
+import com.github.kayvannj.permission_utils.PermissionUtil;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE_SMS = 0;
+    private PermissionUtil.PermissionRequestObject mRequestObject;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRequestObject = PermissionUtil.with(this).request(Manifest.permission.RECEIVE_SMS).onAllGranted(
+                new Func() {
+                    @Override protected void call() {
+                    }
+                }).onAnyDenied(
+                new Func() {
+                    @Override protected void call() {
+                        Toast.makeText(MainActivity.this, "You must allow access to SMS.", Toast.LENGTH_LONG).show();
+                    }
+                }).ask(REQUEST_CODE_SMS);
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        mRequestObject.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
 }
---
+++ app/src/main/java/com/curiosityio/smsreadapp/ReceiveSMSBroadcastReceiver.java
@@ -0,0 +1,38 @@
+package com.curiosityio.smsreadapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.Telephony;
+import android.telephony.SmsMessage;
+import android.util.Log;
+
+public class ReceiveSMSBroadcastReceiver extends BroadcastReceiver {
+
+    @Override public void onReceive(Context context, Intent intent) {
+        if (Telephony.Sms.Intents.SMS_RECEIVED_ACTION.equals(intent.getAction())) {
+            Bundle bundle = intent.getExtras();
+            SmsMessage[] msgs = null;
+            String msg_from;
+            if (bundle != null) {
+                try {
+                    Object[] pdus = (Object[]) bundle.get("pdus");
+                    msgs = new SmsMessage[pdus.length];
+                    for(int i=0; i<msgs.length; i++){
+                        msgs[i] = SmsMessage.createFromPdu((byte[])pdus[i]);
+                        msg_from = msgs[i].getOriginatingAddress();
+                        String msgBody = msgs[i].getMessageBody();
+
+                        if (msgBody.equals("0")) {
+                            Log.d("SUPER_AWESOME_APP", "message received!");
+                        }
+                    }
+                } catch(Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+}
